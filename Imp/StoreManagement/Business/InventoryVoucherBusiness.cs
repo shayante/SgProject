766f976ef1286f9ae608a4c8989dd8aaf8558ca6
ShayanTm <shayan.te@systemgroup.net>
2024-09-24 16:14:04 +0330

    add Businesses

---
+++ Imp/StoreManagement/Business/InventoryVoucherBusiness.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SystemGroup.Framework.Business;
+using SystemGroup.Framework.Common;
+using SystemGroup.Framework.Eventing;
+using SystemGroup.Framework.Exceptions;
+using SystemGroup.Framework.Localization;
+using SystemGroup.Framework.Service;
+using SystemGroup.Framework.Service.Attributes;
+using SystemGroup.Training.StoreManagement.Common;
+
+
+namespace SystemGroup.Training.StoreManagement.Business
+{
+    [Service]
+    public class InventoryVoucherBusiness : BusinessBase<InventoryVoucher>, IInventoryVoucherBusiness
+    {
+    }
+}
