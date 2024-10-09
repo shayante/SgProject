8e4cc4c0cb40355a2e77ca8b3b17d725a4efc6af
ShayanTm <shayan.te@systemgroup.net>
2024-10-09 10:11:37 +0330

    restrict edit units,parts and store when inventory voucher is saved

--- Imp/StoreManagement/Business/InventoryVoucherBusiness.cs
+++ Imp/StoreManagement/Business/InventoryVoucherBusiness.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using SystemGroup.Framework.Business;
-using SystemGroup.Framework.Common;
 using SystemGroup.Framework.Eventing;
-using SystemGroup.Framework.Exceptions;
 using SystemGroup.Framework.Localization;
 using SystemGroup.Framework.Service;
-using SystemGroup.Framework.Service.Attributes;
 using SystemGroup.Training.StoreManagement.Common;
 
 
@@ -17,5 +11,44 @@ namespace SystemGroup.Training.StoreManagement.Business
     [Service]
     public class InventoryVoucherBusiness : BusinessBase<InventoryVoucher>, IInventoryVoucherBusiness
     {
+
+        [SubscribeTo(typeof(IUnitBusiness), "UpdatingRecord", IsAdvisor = true)]
+        private void RestrictEditUnit(object sender, EntitySavingEventArgs<Unit> e)
+        {
+            var hasInventoryVoucher = (from item in FetchDetail<InventoryVoucherItem>()
+                                       join part in ServiceFactory.Create<IPartBusiness>().FetchAll() on item.PartRef equals part.ID
+                                       where part.UnitRef == (long)e.SavedEntity.GetPorpertyOriginalValue("UnitRef")
+                                       select part)
+                                       .Any();
+
+            if (hasInventoryVoucher)
+            {
+                throw this.CreateException("Messages_CannotEditUnitWhenUsedInInventoryVoucherItem");
+            }
+        }
+
+
+        [SubscribeTo(typeof(IPartBusiness), "UpdatingRecord", IsAdvisor = true)]
+        private void RestrictEditPart(object sender, EntitySavingEventArgs<Part> e)
+        {
+            if (FetchDetail<InventoryVoucherItem>().Any(i => i.PartRef == e.SavedEntity.ID))
+            {
+                throw this.CreateException("Messages_CannotEditPartWhenUsedInInventoryVoucherItem");
+            }
+        }
+
+
+
+        [SubscribeTo(typeof(IStoreBusiness), "UpdatingRecord", IsAdvisor = true)]
+        private void RestrictEditStore(object sender, EntitySavingEventArgs<Store> e)
+        {
+            if (FetchAll().Any(i => i.StoreRef == e.SavedEntity.ID))
+            {
+                throw this.CreateException("Messages_CannotEditStoreWhenUsedInInventoryVoucher");
+            }
+        }
+
+
+
     }
 }
