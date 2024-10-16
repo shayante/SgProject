9c96eb7b6ccb3f45734873d0d70d64256e94fa79
ShayanTm <shayan.te@systemgroup.net>
2024-10-16 09:04:08 +0330

    implement convention in pricing

---
+++ Imp/Pricing/Web/Conventions/InventoryVoucherPricing.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using SystemGroup.Framework.Service;
+using SystemGroup.Training.Pricing.Common;
+using SystemGroup.Training.StoreManagement.Common;
+using SystemGroup.Training.StoreManagement.Web.Convention;
+using SystemGroup.Web.UI.Controls;
+using SystemGroup.Web.UI.Localization;
+using Telerik.Web.UI;
+
+namespace SystemGroup.Training.Pricing.Web.Conventions
+{
+    internal class InventoryVoucherPricing : IInventoryVoucherExtraColumn
+    {
+
+        public bool HasColumn(long ivID) =>
+            HasColumn(ServiceFactory.Create<IInventoryVoucherBusiness>().FetchByID(ivID).First());
+
+        public bool HasColumn(InventoryVoucher iv) =>
+            iv.Type == InventoryVoucherType.Enter && iv.State == InventoryVoucherState.Confirmed;
+
+
+        public void AddColumn(SgGrid grid)
+        {
+            var column = new SgDecimalGridColumn
+            {
+                AllowEdit = false,
+                HeaderText = Translator.Translate("Training.Pricing:Labels_Price"),
+                GroupSize = 3,
+                Precision = 2,
+                PropertyName = "ExtraPropery",
+                
+
+            };
+
+            grid.Columns.Add(column);
+            
+
+        }
+
+        public void FillExtraProperies(SgEntityDataSource<InventoryVoucherItem> dataSource)
+        {
+            var priceBiz = ServiceFactory.Create<IItemPriceBusiness>();
+            var ItemIds = dataSource.Entities.Select(i => i.ID);
+            var itemPrices = priceBiz.FetchAll().Where(ip => ItemIds.Contains(ip.VoucherItemRef)).ToDictionary(ip => ip.VoucherItemRef);
+            foreach (var item in dataSource.Entities)
+            {
+                if (itemPrices.ContainsKey(item.ID))
+                {
+                    item.ExtraPropery = itemPrices[item.ID].Price;//.ToString("N2");
+                }
+                
+            }
+        }
+
+        
+    }
+}
